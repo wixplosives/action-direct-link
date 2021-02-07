96c5e220d70969dc84cd877a4013f2e49263874a
alexshe <alexshe@wix.com>
2021-02-07 19:28:41 +0200

    feat: initial implementation

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,56 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
+import {Octokit} from '@octokit/action'
+import {context} from '@actions/github'
+
+
+export function getJobName(job: string, matrixOs: string, matrixNode: string): string {
+  let jobName = job
+  if (matrixOs && matrixNode) {
+    jobName = `${job} (${matrixOs}, ${matrixNode})`
+  } else if (matrixOs && !matrixNode) {
+    jobName = `${job} (${matrixOs})`
+  } else if (!matrixOs && matrixNode) {
+    jobName = `${job} (${matrixNode})`
+  }
+  return jobName
+}
+
+export async function getActionUrl(
+  matrixOs: string,
+  matrixNode: string
+): Promise<string> {
+  const {runId, job} = context
+  const {owner, repo} = context.repo
+
+  const commandUrl =
+    'GET /repos/{onerPar}/{repoName}/actions/runs/{runIdPar}/jobs'
+  const commandParams = {
+    onerPar: owner,
+    repoName: repo,
+    runIdPar: runId
+  }
+  const jobName = getJobName(job, matrixOs, matrixNode)
+  core.info(`Get action logs ${owner}/${repo} ${runId} ${jobName}`)
+  const github_token = process.env['GITHUB_TOKEN']
+  const octokit = new Octokit({auth: github_token})
+  const retval = await octokit.request(commandUrl, commandParams)
+
+  for (const buildNum in retval.data.jobs) {
+    if (retval.data.jobs[buildNum].name === jobName) {
+      const runJobId = retval.data.jobs[buildNum].id
+      const link = `https://github.com/${owner}/${repo}/runs/${runJobId}?check_suite_focus=true`
+      return link
+    }
+  }
+  throw Error(`Job ${jobName} cannot be found at ${owner}/${repo}`)
+}
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
-
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
-
-    core.setOutput('time', new Date().toTimeString())
+    const matrixOs: string = core.getInput('matrix_os')
+    const matrixNode: string = core.getInput('matrix_node')
+    const buildUrl = await getActionUrl(matrixOs, matrixNode)
+    core.setOutput('url', buildUrl)
   } catch (error) {
     core.setFailed(error.message)
   }
